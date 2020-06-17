618fe4c0824260adb971f8c97e5c2b2e14df7f49
Noah Korner <noahskorner@gmail.com>
2020-06-17 12:29:13 -0700

    Dev enviroment setup, django / python app setup. Began working on html template files.

---
+++ store/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def store(request):
+    context = {}
+    return render(request, 'store/store.html')
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html')
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html')
